bf50c66ad4de6c7724dd6e542783efe22d62e897
omar <omarreyes49511@gmail.com>
2021-12-22 03:55:02 -0800

    archivos

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('URL', 'http://localhost/Dentista/');
+
+define('HOST', 'localhost');
+define('DB', 'dentista');
+define('USER', 'root');
+define('PASSWORD', "");
+define('CHARSET', 'utf8mb4');
